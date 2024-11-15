9705f83a3ab33dfa647af2dedd16f469b4e13c1a
Davo0416 <60065631+Davo0416@users.noreply.github.com>
2024-11-15 21:35:54 +0000

    Add files via upload

---
+++ src/Secure_Message_Storage/src/main/java/com/mycompany/messagingappv1/ChatServer.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.messagingappv1;
+
+/**
+ *
+ * @author davog
+ */
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        boolean exit = false;
+        ArrayList<Message> messages;
+         
+        MessageStorage storage = new MessageStorage();
+        storage.CreateFile();
+        messages = storage.ReadMessages();
+        
+        while(!exit){
+            String message = JOptionPane.showInputDialog( "Enter the message that you would like to send. Enter Exit to exit, Enter Clear to clear the storage");
+            
+            if(message.equalsIgnoreCase("EXIT")){
+                exit = true;
+                System.out.println("Exiting ...");
+            }
+            else if(message.equalsIgnoreCase("CLEAR")){
+                storage.ClearFile();
+                System.out.println("Cleared the storage");
+            }
+            else{
+                Message msg = new Message("Sender", "Reciever", message, "message");
+                storage.SaveMessage(msg);
+                System.out.println(message);
+                messages.add(msg);
+            }
+        }
+
+        
+        
+        
+        //storage.ClearFile();
+        
+    }
+}
---
+++ src/Secure_Message_Storage/src/main/java/com/mycompany/messagingappv1/MessageStorage.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.messagingappv1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+/**
+ *
+ * @author davog
+ */
+public class MessageStorage {
+    String key = "superDuperSecureSecretKey";
+    SecretKey myDesKey;
+    Cipher desCipher;
+
+    
+    public MessageStorage(){
+        try
+        {
+            // Generating objects of KeyGenerator & SecretKey and setring up the encryption / decryption
+            KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+            myDesKey = keygenerator.generateKey();
+            
+            byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+            DESKeySpec desKeySpec = new DESKeySpec(keyBytes);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            myDesKey = keyFactory.generateSecret(desKeySpec);
+            desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    //Encript the given string using the DES key
+    public String Encrypt(String message) {
+        try
+        {
+            desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+            // Encrypt the message
+            byte[] encryptedData = desCipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
+            // Encode the encrypted data to base64 to write to the file
+            return Base64.getEncoder().encodeToString(encryptedData);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+    //Decript the given string using the DES key
+    public String Decrypt(String cypher){
+        try
+        {
+            desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+            // Decode the base64-encoded ciphertext
+            byte[] encryptedData = Base64.getDecoder().decode(cypher);
+            // Decrypt the data
+            byte[] decryptedData = desCipher.doFinal(encryptedData);
+            return new String(decryptedData, StandardCharsets.UTF_8);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+    //Read all the messages from the text file and Decrypt them into an ArrayList<Message>
+    public ArrayList<Message> ReadMessages() {
+        ArrayList<Message> messages = new ArrayList<Message>();
+        
+        try {
+            File messageData = new File("MessageData.txt");
+            Scanner myReader;
+            myReader = new Scanner(messageData);
+            //Read lines one by one and decrypt them
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                data = Decrypt(data);
+                Message msg =Message.fromJson(data);
+                System.out.println(msg.getContent());
+                messages.add(msg);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while reading the file");
+            e.printStackTrace();
+        }
+        
+        return messages;
+    }
+    
+    //Save the given message to the text file after encripting it 
+    public void SaveMessage(Message msg) {
+        try {
+            FileWriter myWriter = new FileWriter("MessageData.txt", true);
+            myWriter.write(Encrypt(msg.toJson()));
+            myWriter.write('\n');
+            
+            myWriter.flush();
+            myWriter.close();
+            
+
+        } catch (IOException e) {
+            System.out.println("Error occurred while writing to fie ");
+            e.printStackTrace();
+        }
+    }
+    
+    //Create the text file if it doesnt exist 
+    public void CreateFile() {
+        try {
+            File myObj = new File("MessageData.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating a new file.");
+            e.printStackTrace();
+        }
+    }
+    
+    //Clear all the data on the file
+    public void ClearFile() {
+        try {
+            FileWriter myWriter = new FileWriter("MessageData.txt", false);
+            myWriter.write("");
+        } catch (IOException e) {
+            System.out.println("An error occurred while clearing the file");
+            e.printStackTrace();
+        }
+    }
+}
